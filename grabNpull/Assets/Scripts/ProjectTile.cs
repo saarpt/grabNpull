e80a60e923a35bdc4fa0233a129bb48f2f99c043
Sarper <saarptemel@gmail.com>
2018-10-17 11:11:55 +0200

    2dGame"
    2dGame

---
+++ grabNpull/Assets/Scripts/ProjectTile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectTile : MonoBehaviour {
+
+
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * speed * Time.deltaTime); 	
+	}
+}
